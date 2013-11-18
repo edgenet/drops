3c09fd84968fa1d844a0dd2c26739a3ff3c6bcfd
Shannen Saez <shannenlaptop@gmail.com>
2013-11-19 02:17:18 +1100

    Fix build--include header files.
    Requires that dependencies reside in the project directory.
    
    Signed-off-by: Shannen Saez <shannenlaptop@gmail.com>

--- src/app/jni/Android.mk
+++ src/app/jni/Android.mk
@@ -4,6 +4,11 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := drops-jni
 LOCAL_SRC_FILES := drops-jni.c
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../include
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../../zyre/include
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../../czmq/include
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../../libzmq/include
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../../libsodium/src/libsodium/include/
 LOCAL_LD_LIBS := -lsodium -lzmq -lczmq -lzyre -ldrops
 
 include $(BUILD_SHARED_LIBRARY)
