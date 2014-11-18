98ee0a01efd61ed0b4c338c7dbaf8806ce01234b
Joe Eli McIlvain <joe.eli.mac@gmail.com>
2014-11-17 22:00:51 -0800

    Problem: No generated Ruby bindings Solution: Generate Ruby bindings

---
+++ bindings/ruby/spec/spec_helper.rb
@@ -0,0 +1,8 @@
+
+require 'drops/ffi'
+
+RSpec.configure do |c|
+  # Enable 'should' syntax
+  c.expect_with(:rspec) { |c| c.syntax = [:should, :expect] }
+  c.mock_with(:rspec)   { |c| c.syntax = [:should, :expect] }
+end
